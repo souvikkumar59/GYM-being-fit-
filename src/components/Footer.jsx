a42fde504563ff9ab2208a7cf5738c00a3dde93f
SOUVIK KUMAR BAGULI <souvikkumarbaguli51@gmail.com>
2024-12-21 17:55:06 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        &copy; 2024 souvikkumarbaguli (BEING FIT). All rights reserved.
+      &copy;
+    </footer>
+  )
+}
+
+export default Footer
